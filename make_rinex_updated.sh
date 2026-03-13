5867e7a9e620c5546d5166612c4e633da3e1a825
Oleksandr Koloskov <alex.koloskov@unb.ca>
2026-03-13 16:59:08 +0000

    convbin version 2.4.3 without -ti key

--- make_rinex_updated.sh
+++ make_rinex_updated.sh
@@ -261,9 +261,13 @@ if [[ "${QUARTER}" -eq 0 ]]; then # CONVBIN run at startup
         log -l 0 -m "Runing CONVBIN to convert ${BINFILE} to RINEX" -o ""
 
         # Run CONVBIN for processing at startup
-        ~/bin/convbin -ti 1.0000 -tt 0.05 -ro "-TADJ=1.0" -tr "${YEAR}/${MONTH}/${DAY} 00:00:00" -od -os -v 3.04 \
-                      -hm "${STATION}" -o "${TEMP_OUT}" "${BINFILE}" 2>${TEMP_DIR}/convbin.log
-#        ~/bin/convbin -od -os -v 3.04 -hm "${STATION}" -o "${TEMP_OUT}" "${BINFILE}" 2>${TEMP_DIR}/convbin.log
+################### VERSION for 2.5.0 to align time with top of second
+#        ~/bin/convbin250 -ti 1.0000 -tt 0.05 -ro "-TADJ=1.0" -tr "${YEAR}/${MONTH}/${DAY} 00:00:00" -od -os -v 3.04 \
+#                         -hm "${STATION}" -o "${TEMP_OUT}" "${BINFILE}" 2>${TEMP_DIR}/convbin.log
+################### VERSION for 2.4.3 with -ti 1.0000 as in original script
+#        ~/bin/convbin -ti 1.0000 -od -os -v 3.04 -hm "${STATION}" -o "${TEMP_OUT}" "${BINFILE}" 2>${TEMP_DIR}/convbin.log
+################### VERSION for 2.4.3 without -ti option
+        ~/bin/convbin -od -os -v 3.04 -hm "${STATION}" -o "${TEMP_OUT}" "${BINFILE}" 2>${TEMP_DIR}/convbin.log
 
         # Error if CONVBIN fail
             if [[ $? -ne 0 ]]; then
@@ -334,11 +338,16 @@ else # CONVBIN run in loop
         log -l 0 -m "Runing CONVBIN to convert ${BINFILE} to RINEX" -o ""
 
         # Run CONVBIN for processing in loop mode
+################### VERSION for 2.5.0 to align time with top of second
+#        ~/bin/convbin250 -ts "${START_DATE}" "${START_TIME}" -te "${STOP_DATE}" "${STOP_TIME}" \
+#                      -ti 1.0000 -tt 0.05 -ro "-TADJ=1.0" -tr "${YEAR}/${MONTH}/${DAY} 00:00:00" -od -os -v 3.04 \
+#                      -hm "${STATION}" -o "${TEMP_OUT}" "${BINFILE}" 2>${TEMP_DIR}/convbin.log
+################### VERSION for 2.4.3 with -ti 1.0000 as in original script
+#            ~/bin/convbin -ts "${START_DATE}" "${START_TIME}" -te "${STOP_DATE}" "${STOP_TIME}" \
+#                      -ti 1.0000 -od -os -v 3.04 -hm "${STATION}" -o "${TEMP_OUT}" "${BINFILE}" 2>${TEMP_DIR}/convbin.log
+################### VERSION for 2.4.3 without -ti option
         ~/bin/convbin -ts "${START_DATE}" "${START_TIME}" -te "${STOP_DATE}" "${STOP_TIME}" \
-                      -ti 1.0000 -tt 0.05 -ro "-TADJ=1.0" -tr "${YEAR}/${MONTH}/${DAY} 00:00:00" -od -os -v 3.04 \
-                      -hm "${STATION}" -o "${TEMP_OUT}" "${BINFILE}" 2>${TEMP_DIR}/convbin.log
-#        ~/bin/convbin -ts "${START_DATE}" "${START_TIME}" -te "${STOP_DATE}" "${STOP_TIME}" \
-#                      -od -os -v 3.04 -hm "${STATION}" -o "${TEMP_OUT}" "${BINFILE}" 2>${TEMP_DIR}/convbin.log
+                      -od -os -v 3.04 -hm "${STATION}" -o "${TEMP_OUT}" "${BINFILE}" 2>${TEMP_DIR}/convbin.log
 
         # Error if CONVBIN fail
         if [[ $? -ne 0 ]]; then
@@ -662,7 +671,7 @@ fi
 
 # Cleanup temp files
 log -l 0 -m  "Remove the temporary ${TEMP_DIR} folder" -o ""
-#rm -rf "${TEMP_DIR}"
+rm -rf "${TEMP_DIR}"
 if [[ $? -ne 0 ]]; then
     log -l 3 -m  "ERROR: $? Cannot remove the temporary ${TEMP_DIR} folder: EXIT" -o ""
     exit 19
